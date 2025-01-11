faabe87a8e6dd39f747bf596a4ba08f5f615e558
developer1Mtechub <developer1.mtechub@gmail.com>
2025-01-11 21:36:25 +0500

    Finalizing

--- src/utils/globalVariables.js
+++ src/utils/globalVariables.js
@@ -13,8 +13,8 @@ export const firebase_server_key =
 // export const BASE_URL_IMAGE = 'http://192.168.57.33:3017/';
 
   // Room
-export const BASE_URL = 'http://192.168.100.36:3017/';
-export const BASE_URL_IMAGE = 'http://192.168.100.36:3017/';
+// export const BASE_URL = 'http://192.168.100.36:3017/';
+// export const BASE_URL_IMAGE = 'http://192.168.100.36:3017/';
 
   // Ayyaz
 // export const BASE_URL = 'http://192.168.8.101:3017/';
@@ -24,8 +24,8 @@ export const BASE_URL_IMAGE = 'http://192.168.100.36:3017/';
 // export const BASE_URL = 'https://food-delivery-be.caprover-demo.mtechub.com/';
 // export const BASE_URL_IMAGE = 'https://food-delivery-be.caprover-demo.mtechub.com/';
 
-// export const BASE_URL = 'https://food-delivery-backend-testing.caprover.musuvillc.com/';
-// export const BASE_URL_IMAGE = 'https://food-delivery-backend-testing.caprover.musuvillc.com/';
+export const BASE_URL = 'https://food-delivery-backend-testing.caprover.musuvillc.com/';
+export const BASE_URL_IMAGE = 'https://food-delivery-backend-testing.caprover.musuvillc.com/';
 
 
 // Office
